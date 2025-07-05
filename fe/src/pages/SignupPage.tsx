aac6c18ffa07211755d2ce6aa913f4f221f4762e
AakashIND <aakash142017@gmail.com>
2025-07-06 00:44:54 +0530

    Remove node_modules from Git tracking and update .gitignore

--- fe/src/pages/SignupPage.tsx
+++ fe/src/pages/SignupPage.tsx
@@ -1,1 +1,13 @@
+ import { useEffect , useState } from "react";
+
+
+ export default function SignupPage(){
+    return (
+        <div className = "container mx-auto ">
+            <h1>Signup</h1>
+        </div>
+    )
+ }
+
  
+const isLoggedIn = 
